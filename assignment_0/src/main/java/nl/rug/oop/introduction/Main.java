c40759fee3d95d54633621129bcfa31c98f6e985
Annika Möller <annika.moeller24@gmail.com>
2020-04-18 00:48:07 +0800

    assignment 0

--- assignment_0/src/main/java/nl/rug/oop/introduction/Main.java
+++ assignment_0/src/main/java/nl/rug/oop/introduction/Main.java
@@ -1,3 +1,54 @@
 package nl.rug.oop.introduction;
+import java.util.*;
 
-// Add your code here.
+public class Main {
+
+    private static Animal[] initAnimals() {
+
+        Animal dog = new Animal("Dog", 8);
+        Animal ant = new Animal("Ant", 2);
+        Animal lion = new Animal("Mufasa", 600);
+
+        Animal[] animals = {dog, ant, lion};
+        return animals;
+    }
+
+    private static Animal[] initAnimals(String[] args) {
+        Animal[] animals = new Animal[args.length];
+        for (int i = 0; i < args.length; i++) {
+            String name = args[i];
+            animals[i] = new Animal(name);
+        }
+        return animals;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        Animal[] animals;
+
+        if (args.length == 0) {
+            animals = initAnimals();
+        } else {
+            animals = initAnimals(args);
+        }
+
+        while (scanner.hasNextLine()) {
+
+            String line = scanner.nextLine();
+
+            for (Animal animal : animals) {
+                if (line.equals("eat")) {
+                    animal.eat();
+                } else if (line.equals("run")) {
+                    animal.run();
+                }
+                animal.printHungerLevel();
+            }
+        }
+    }
+}
+
+
+
