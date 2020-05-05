0af79330ae9a793be426f8fff7b2077488103a29
Andrei <lucianandrei12@gmail.com>
2020-05-05 13:07:22 +0200

    some warnings

--- assignment_1/src/main/java/nl/rug/oop/rpg/io/Serializer.java
+++ assignment_1/src/main/java/nl/rug/oop/rpg/io/Serializer.java
@@ -1,9 +1,5 @@
 package nl.rug.oop.rpg.io;
-import nl.rug.oop.rpg.Game;
 import nl.rug.oop.rpg.Player;
-import nl.rug.oop.rpg.Room;
-import nl.rug.oop.rpg.doors.Door;
-import nl.rug.oop.rpg.npcs.NPC;
 
 import java.io.*;
 import java.nio.file.Files;
@@ -23,10 +19,7 @@ public class Serializer {
 
         /* write object to a file */
         try(FileOutputStream fileOutputStream = new FileOutputStream(saveDirectory + File.separator + fileName + ".ser");
-            ObjectOutputStream playerOutputStream = new ObjectOutputStream(fileOutputStream);
-            /*ObjectOutputStream roomOutputStream = new ObjectOutputStream(fileOutputStream);
-            ObjectOutputStream doorOutputStream = new ObjectOutputStream(fileOutputStream);
-            ObjectOutputStream npcOutputStream = new ObjectOutputStream(fileOutputStream)*/) {
+            ObjectOutputStream playerOutputStream = new ObjectOutputStream(fileOutputStream)) {
             playerOutputStream.writeObject(player);
             System.out.println("Save successful!");
         } catch (FileNotFoundException e) {
@@ -62,5 +55,4 @@ public class Serializer {
         }
         return null;
     }
-
 }
