77752ee436f3d9bdca58716ae5c0627ae785b328
Annika Möller <annika.moeller24@gmail.com>
2020-05-07 14:53:37 +0800

    made function for user to name player when initialising from config

--- assignment_1/src/main/java/nl/rug/oop/rpg/io/Initialiser.java
+++ assignment_1/src/main/java/nl/rug/oop/rpg/io/Initialiser.java
@@ -1,5 +1,6 @@
 package nl.rug.oop.rpg.io;
 
+import nl.rug.oop.rpg.Main;
 import nl.rug.oop.rpg.Player;
 import nl.rug.oop.rpg.Room;
 import nl.rug.oop.rpg.doors.Door;
@@ -10,19 +11,50 @@ import nl.rug.oop.rpg.npcs.NPC;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class Initialiser {
+
+    private static String namePlayer(){
+        String name = Main.scanner.nextLine();
+        if (name.equals("!")){
+            name = "Bob";
+        }
+        if (SaveAndLoad.isAlphaNumeric(name) &&
+                name.length() > 0 && name.length() < 21) {
+            return name;
+        }
+        else {
+            System.out.println("Name does not meet requirements, try again (! to skip):");
+            namePlayer();
+        }
+        return name;
+    }
+
     public static void createProperties(String fileName){
+        Main.scanner = new Scanner(System.in);
+        System.out.println("Configure player's name (! to skip):");
+        String playerName = namePlayer();
+
+        /*System.out.println("Set player's max health:");
+        int maxHealth = Main.scanner.nextInt();
+
+        System.out.println("Set difficulty level (default = medium):");
+        int diffLevel = Main.scanner.nextInt();
+
+        System.out.println("Set the power door's power level:");
+        int doorPower = Main.scanner.nextInt();*/
+
         int maxHealth = 50;
-        int enemyDamage = 200;
-        int enemyHealth = 500;
+        int enemyDamage = 20;
+        int enemyHealth = 30;
         int doorPower = 30;
 
         File configDirectory = new File("config");
         configDirectory.mkdir();
 
         Properties gameProperties = new Properties();
-        gameProperties.setProperty("playerName", "jake");
+        gameProperties.setProperty("playerName", playerName);
         gameProperties.setProperty("maxHealth", String.valueOf(maxHealth));
         gameProperties.setProperty("enemyDamage", String.valueOf(enemyDamage));
         gameProperties.setProperty("enemyHealth", String.valueOf(enemyHealth));
