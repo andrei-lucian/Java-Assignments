fe5addcf3e2cd9edab093fa9450087d7db07f003
Andrei <lucianandrei12@gmail.com>
2020-05-09 14:53:06 +0200

    hello world

--- assignment_2/src/main/java/nl/rug/oop/cardgame/Main.java
+++ assignment_2/src/main/java/nl/rug/oop/cardgame/Main.java
@@ -1,7 +1,8 @@
-package nl.rug.oop.cardgame;
+package main.java.nl.rug.oop.cardgame;
 
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("hello");
     }
 }
