b7380104fdfd3f7a29f54e32d8e8772dbb1cf4a8
Andrei <lucianandrei12@gmail.com>
2020-05-22 17:16:30 +0200

    comments

--- assignment_2/src/main/java/nl/rug/oop/cardgame/model/CurrentCard.java
+++ assignment_2/src/main/java/nl/rug/oop/cardgame/model/CurrentCard.java
@@ -4,7 +4,6 @@ import nl.rug.oop.cardgame.model.elements.Card;
 
 /** Class to store the state of the imaginary card
  * that a participant has to compare their cards to */
-
 public class CurrentCard {
     private Card.Face face;
     private Card.Suit suit;
