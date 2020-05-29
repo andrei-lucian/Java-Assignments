220921690375ecdb178884eb0a49ee27c3779e4b
Andrei <lucianandrei12@gmail.com>
2020-05-29 15:53:16 +0200

    added button to add nodes

--- assignment_3/src/main/java/nl/rug/oop/grapheditor/controller/actions/AddNodeAction.java
+++ assignment_3/src/main/java/nl/rug/oop/grapheditor/controller/actions/AddNodeAction.java
@@ -1,4 +1,29 @@
 package nl.rug.oop.grapheditor.controller.actions;
 
-public class AddNodeAction {
+import nl.rug.oop.grapheditor.model.GraphModel;
+import nl.rug.oop.grapheditor.model.Node;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.util.Observable;
+import java.util.Observer;
+
+public class AddNodeAction extends AbstractAction implements Observer {
+
+    private GraphModel graph;
+
+    public AddNodeAction(GraphModel graph){
+        super("Add a node");
+        graph.addObserver(this);
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        graph.addNode(new Node());
+    }
 }
