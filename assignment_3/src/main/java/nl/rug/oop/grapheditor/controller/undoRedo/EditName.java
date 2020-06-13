95b6a9f8cd0d68f92dd542f7ddbb5383584cffb4
Annika Möller <annika.moeller24@gmail.com>
2020-06-13 19:16:29 +0200

    can change node name

---
+++ assignment_3/src/main/java/nl/rug/oop/grapheditor/controller/undoRedo/EditName.java
@@ -0,0 +1,40 @@
+package nl.rug.oop.grapheditor.controller.undoRedo;
+
+import nl.rug.oop.grapheditor.model.GraphModel;
+import nl.rug.oop.grapheditor.model.Node;
+
+import javax.swing.*;
+import javax.swing.undo.AbstractUndoableEdit;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+
+public class EditName extends AbstractUndoableEdit {
+
+    private GraphModel graph;
+    private Node node;
+
+    public EditName(GraphModel graph){
+        this.graph = graph;
+    }
+
+    @Override
+    public void undo() throws CannotUndoException {
+        super.undo();
+    }
+
+    @Override
+    public void redo() throws CannotRedoException {
+        if(!canRedo()) {
+            this.node = graph.getSelectedNode();
+            String newName = (String) JOptionPane.showInputDialog(
+                    null,
+                    "Input a new name:",
+                    "Edit Node Name");
+            node.setName(newName);
+        }
+
+        else {
+            super.redo();
+        }
+    }
+}
